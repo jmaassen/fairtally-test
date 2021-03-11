df7603ebf641ea8dd8c21173079e4a545c871121
Jason Maassen <jmaassen73@gmail.com>
2021-03-11 10:15:23 +0100

    Added test script for env

--- run-fairtally.sh
+++ run-fairtally.sh
@@ -2,16 +2,13 @@
 
 # Set relevant enviroment variable is set
 
-export APIKEY_GITHUB=$1
+export APIKEY_GITHUB="$1"
 
-if [[ ! -v "$APIKEY_GITHUB" ]]; then
+echo "APIKEY_GITHUB is set to $APIKEY_GITHUB"
+
+if [[ -z "$APIKEY_GITHUB" ]]; then
     echo "APIKEY_GITHUB is not set"
     exit 1
-elif [[ -z "$APIKEY_GITHUB" ]]; then
-    echo "APIKEY_GITHUB is set to the empty string"
-    exit 1
-else
-    echo "APIKEY_GITHUB has the value: $DEPLOY_ENV"
 fi
 
 fairtally -o reports/report-${{steps.timestamp.outputs.date}}.html -i urls.txt
