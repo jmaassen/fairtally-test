3c811e7ee56d14df651b0638bc5311a44e26c5de
Jason Maassen <jmaassen73@gmail.com>
2021-03-11 09:54:11 +0100

    Added test script for env

---
+++ run-fairtally.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if the relevant enviroment variable is set
+
+if [[ ! -v "$APIKEY_GITHUB" ]]; then
+    echo "APIKEY_GITHUB is not set"
+    exit 1
+elif [[ -z "$APIKEY_GITHUB" ]]; then
+    echo "APIKEY_GITHUB is set to the empty string"
+    exit 1
+else
+    echo "APIKEY_GITHUB has the value: $DEPLOY_ENV"
+fi
+
