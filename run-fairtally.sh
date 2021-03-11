6a9e3890d00cd8d7425c98c1cd18b1058a0c3853
Jason Maassen <jmaassen73@gmail.com>
2021-03-11 13:39:38 +0100

    moved output dir

--- run-fairtally.sh
+++ run-fairtally.sh
@@ -20,6 +20,6 @@ fi
 #cat software.json | jq -r '[.[].repositoryURLs.github] | flatten | .[]' > urls.>
 
 # Run fairtally
-fairtally -o reports/report-$TIMESTAMP.html -i urls.txt
+fairtally -o doc/reports/report-$TIMESTAMP.html -i urls.txt
 
 
